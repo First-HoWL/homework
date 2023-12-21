178ad266d9473bc10cae029e981effa2cb538e31
User <d24e09n20i09s@ukr.net>
2023-12-21 13:31:34 +0200

    Домашне завдання

---
+++ main.py
@@ -0,0 +1,25 @@
+'''
+1
+a = int(input('Перше число'))
+b = int(input('Друге число'))
+c = int(input('третє число'))
+print(a + b + c)
+print(a * b * c)
+'''
+
+'''
+2
+a = int(input('Зарплата'))
+b = int(input('Місячний платіж в банк'))
+c = int(input('Комунальні послуги'))
+print(a - b - c)
+
+'''
+'''
+3
+
+a = int(input('Перша діагональ'))
+b = int(input('Друга діагональ'))
+print((a * b) / 2)
+
+'''
