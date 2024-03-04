109e2048f4ca7c38aae4c1b965d514b752585fd6
User <d24e09n20i09s@ukr.net>
2024-03-04 12:59:08 +0200

    Домашне завдання

--- hamework 13.03.py
+++ hamework 13.03.py
@@ -7,7 +7,7 @@ response = requests.get(url)
 currensies_data = response.json()['data']
 if response.ok:
     print(f"Ім\'я: {currensies_data['name']}, фільми: {currensies_data['films']}, короткі фильми: {currensies_data['shortFilms']}, ТВ-шоу "
-          f"{currensies_data['tvShows']}. Ігри: {currensies_data['videoGames']}".replace("[]", "---"))
+          f"{currensies_data['tvShows']}, Ігри: {currensies_data['videoGames']}.".replace("[]", "---"))
 else:
     print('Щось пішло не так...')
     print(f'{response.status_code=}')
